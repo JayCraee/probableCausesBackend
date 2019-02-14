2c560d4ea8d902454d2ef41042f6bf2e1da6d696
D.K. Saaw <dks28@thunderbird-2.linux.ds.cam.ac.uk>
2019-02-14 19:20:04 +0000

    Instantiate first version of the call script.

---
+++ src/main/java/partib/groupProject/probableCauses/backend/controller/InvalidCallException.java
@@ -0,0 +1,7 @@
+package partib.groupProject.probableCauses.backend.controller;
+
+public class InvalidCallException extends Exception{
+	public InvalidCallException(String msg){
+		super(msg);
+	}
+}
--- src/main/java/partib/groupProject/probableCauses/backend/controller/ServerConnector.java
+++ src/main/java/partib/groupProject/probableCauses/backend/controller/ServerConnector.java
@@ -1,40 +1,78 @@
 package partib.groupProject.probableCauses.backend.controller;
 
 import java.io.*;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
+import java.util.ArrayList;
 
 public class ServerConnector {
-    private static String server_ip = ""; //TODO try not to push with the server_ip there
 
-    public static String singleQueryCaller(String s) {
-        try {
-            URL url = new URL("http://"+ server_ip + ":8080");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");  // TODO Make it a POST
-            con.setRequestProperty("", ""); // TODO put it in the same format we use
+	private static String server_ip = ""; //TODO try not to push with the server_ip there
+	
+	public static String singleQueryCaller(String db, String query) {
+		List<String> q = new ArrayList();
+		q.add(query);
+		return queryCaller(db, q);
+	}
+	
+	public static String singleQueryCaller(String q){
+		return singleQueryCaller("foo.bdb", q);
+	}
 
-            //int responseCode = con.getResponseCode();
+	public static String queryCaller(String db, List<String> queries){
+		String response = "";
+		try {
+			URL url = new URL("http://"+ server_ip + ":8080");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST"); 
+			
+			if(queries.isEmpty()) throw new InvalidCallException("No queries provided.");
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLIne;
-            StringBuilder response = new StringBuilder();
-            while((inputLIne = br.readLine()) != null) {
-                response.append(inputLIne);
-            }
-            br.close();
-            con.disconnect();
+			String msg = db + "\n";
+			
+			for(String q : queries){
+				msg += q + "\n";
+			}
 
-            return response.toString();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+			con.setDoOutput(true);
+			
+			OutputStream os = con.getOutputStream();
+			try{
+				os.write(msg.getBytes());
+			} catch (IOException io){
+				System.out.println("Caught IOException.");
+			} finally {
+				os.flush();
+				os.close();
+			}
 
-    public static List<String> queryCaller(List<String> list) { //TODO do this method also
-        for(int i = 0; i < list.size(); i++) list.set(i, singleQueryCaller(list.get(i)));
-        return list;
-    }
+			int rc = con.getResponseCode();
+			response = "";
+			String tmp;
+			switch(rc){
+			
+				case HttpURLConnection.HTTP_OK:
+					BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					while((tmp=in.readLine()) != null) response += tmp;
+					break;
+				default:
+					System.out.println("Something went wrong!");
+					break;
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InvalidCallException i) {
+			System.out.println(i.getMessage());
+		}
+
+		return response; //TODO: See if I can just return the string rather than a list of strings -- non-trivial to parse due to CF property of JSON.
+	}
+
+	public static String queryCaller(List<String> queries) { 
+		return queryCaller("foo.bdb", queries);
+	}
 }
