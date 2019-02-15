1f0dd07049836f001fa443e06b838347d3297c44
dks28 <dks28@cam.ac.uk>
2019-02-15 09:47:32 +0000

    Add unit tests for ServerConnector.

--- src/main/java/partib/groupProject/probableCauses/backend/controller/ServerConnector.java
+++ src/main/java/partib/groupProject/probableCauses/backend/controller/ServerConnector.java
@@ -1,5 +1,16 @@
 package partib.groupProject.probableCauses.backend.controller;
 
+/**
+*   ServerConnector.java
+*   @author dks28
+*   
+*   Provides a Java API to connect to the data base server with queries.
+*   The current state is bad Java practice; shouldn't be a collection of static methods, 
+*   rather should provide constructors for ServerConnector objects that then take information 
+*   on server address and port, such that they don't need to be hard coded.
+*/
+
+
 import java.io.*;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
@@ -10,19 +21,39 @@ import java.util.ArrayList;
 public class ServerConnector {
 
 	private static String server_ip = ""; //TODO try not to push with the server_ip there
-	
-	public static String singleQueryCaller(String db, String query) {
+
+	public static boolean isStringNullOrWhiteSpace(String value) {
+		if (value == null) {
+			return true;
+		}
+
+		for (int i = 0; i < value.length(); i++) {
+			if (!Character.isWhitespace(value.charAt(i))) {
+				return false;
+			}
+		}
+		return true;
+	}	
+
+	public static String singleQueryCaller(String db, String query) throws InvalidCallException{
 		List<String> q = new ArrayList();
 		q.add(query);
 		return queryCaller(db, q);
 	}
 	
-	public static String singleQueryCaller(String q){
-		return singleQueryCaller("foo.bdb", q);
+	public static String singleQueryCaller(String q) throws InvalidCallException{
+		throw new InvalidCallException("This method is deprecated. " + 
+			"Please provide a data base file to operate on and call singleQueryCaller(String db, String query)");
 	}
 
-	public static String queryCaller(String db, List<String> queries){
+	public static String queryCaller(String db, List<String> queries) throws InvalidCallException {
 		String response = "";
+		// Filter out empty queries
+		List<String> tmp = new ArrayList(queries);
+		for(String q : tmp){
+			if(isStringNullOrWhiteSpace(q)) queries.remove(q);
+		}
+
 		try {
 			URL url = new URL("http://"+ server_ip + ":8080");
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -30,10 +61,10 @@ public class ServerConnector {
 			
 			if(queries.isEmpty()) throw new InvalidCallException("No queries provided.");
 
-			String msg = db + "\n";
+			String msg = db;
 			
 			for(String q : queries){
-				msg += q + "\n";
+				msg += "\n" + q;
 			}
 
 			con.setDoOutput(true);
@@ -50,12 +81,12 @@ public class ServerConnector {
 
 			int rc = con.getResponseCode();
 			response = "";
-			String tmp;
+			String tmpstr;
 			switch(rc){
 			
 				case HttpURLConnection.HTTP_OK:
 					BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-					while((tmp=in.readLine()) != null) response += tmp;
+					while((tmpstr=in.readLine()) != null) response += tmpstr;
 					break;
 				default:
 					System.out.println("Something went wrong!");
@@ -65,14 +96,13 @@ public class ServerConnector {
 
 		} catch (IOException e) {
 			e.printStackTrace();
-		} catch (InvalidCallException i) {
-			System.out.println(i.getMessage());
 		}
 
-		return response; //TODO: See if I can just return the string rather than a list of strings -- non-trivial to parse due to CF property of JSON.
+		return response; 
 	}
 
-	public static String queryCaller(List<String> queries) { 
-		return queryCaller("foo.bdb", queries);
+	public static String queryCaller(List<String> queries) throws InvalidCallException{ 
+		throw new InvalidCallException("This method is deprecated. " + 
+			"Please provide a data base file to operate on and call queryCaller(String db, List<String> queries)");
 	}
 }
---
+++ src/main/java/partib/groupProject/probableCauses/backend/unitTests/TestServerConnector.java
@@ -0,0 +1,132 @@
+package partib.groupProject.probableCauses.backend.unitTests;
+
+import partib.groupProject.probableCauses.backend.controller.ServerConnector;
+import partib.groupProject.probableCauses.backend.controller.InvalidCallException;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TestServerConnector{
+
+	public void testDeprSingleQuery(){
+		
+		System.out.println("Testing deprecated single query call method.");
+
+		boolean threwException = false;
+		String testQuery = "DROP *;";
+		try{
+			ServerConnector.singleQueryCaller(testQuery);
+		} catch (InvalidCallException inv) {
+			threwException = true;
+		}
+		assert threwException : "Failed test: Did not throw Exception when calling invalidated method singleQueryCaller(String).";
+	}
+
+	public void testDeprMultQuery(){
+		
+		System.out.println("Testing deprecated multiple query call method.");
+
+		boolean threwException = false;
+		String testQuery = "DROP *;";
+		List<String> testQueries= new ArrayList();
+		testQueries.add(testQuery);
+		try{
+			ServerConnector.queryCaller(testQueries);
+		} catch (InvalidCallException inv) {
+			threwException = true;
+		}
+		assert threwException : "Failed test: Did not throw Exception when calling invalidated method queryCaller(List<String>).";
+	}
+
+	public void testRejectNoQueries(){
+		
+		System.out.println("Testing wether rejecting empty queries fails.");
+		
+		/* Tests whether the class rejects the follwoing scenarios: 
+			empty list given, 
+			only empty strings given,
+			list with only spaces given, 
+			list with only tabs given, 
+			list with only newlines given, 
+			tests with mixed whitespace given.
+		*/
+		
+		boolean threwExceptionsOnAll = true;
+
+		List<String> emptyList=new ArrayList();
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", emptyList);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on empty list.";
+
+		List<String> onlyEmpty = Arrays.asList("", "", "");
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", onlyEmpty);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on list of empty Strings.";
+
+		List<String> onlySpaces = Arrays.asList("  ", "   ", " ");
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", onlySpaces);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on list of space Strings.";
+
+		List<String> onlyTabs = Arrays.asList("\t\t\t", "\t", "\t\t");
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", onlyTabs);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on list of tab Strings.";
+
+		List<String> onlyNewline = Arrays.asList("\n\n\n", "\n", "\n\n");
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", onlyNewline);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on list of newline Strings.";
+
+		List<String> mixedWhiteSpace = Arrays.asList(" \t\n \n\n", "  \t\t\t \n","     \t");
+		try{
+			threwExceptionsOnAll = false;
+			ServerConnector.queryCaller("foo.bdb", mixedWhiteSpace);
+		} catch (InvalidCallException inv) {
+			threwExceptionsOnAll = true;
+		}
+
+		assert threwExceptionsOnAll : "Did not throw Exception on list of whitespace Strings.";
+
+	}
+
+	public void testConnection(){
+		
+		System.out.println("Testing that a connection works.");
+
+		List<String> queries = Arrays.asList("SQL CREATE TABLE t (name TEXT, x INT, y INT);", "	SQL INSERT INTO t VALUES(\"A\", 1, 2);", "SQL INSERT INTO t VALUES(\"B\", 3, 4);", "SELECT * FROM t;", "DROP TABLE t;");
+		try {
+			ServerConnector.queryCaller("foo.bdb", queries);
+		} catch (Exception e) {
+			e.printStackTrace();
+			assert false : "Caught Exception in method execution.";
+		}
+
+	}
+}
