b51f9dbc1c514339675003fc94c6145d923ca445
Dan <djh242@cam.ac.uk>
2019-02-20 21:50:13 +0000

    Add tests for Simulate

--- src/test/java/unitTests/AllQueryTests.java
+++ src/test/java/unitTests/AllQueryTests.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-        estimateTest.class,
+        EstimateTest.class,
         SimulateTest.class}) // TODO add: findOutliersTest, inferTest, repairTest, selectTest
 
 public class AllQueryTests {
