6b942f72b45dee60cef7dc2bd1aac67f7cb73510
unknown <aw764@cam.ac.uk>
2019-02-20 17:46:07 +0000

    Edited Simulate NewFieldName.

--- src/main/java/partib/groupProject/probableCauses/backend/model/bql/query/Simulate.java
+++ src/main/java/partib/groupProject/probableCauses/backend/model/bql/query/Simulate.java
@@ -3,7 +3,6 @@ package partib.groupProject.probableCauses.backend.model.bql.query;
 import java.lang.reflect.MalformedParametersException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 public class Simulate extends Query {
@@ -35,8 +34,9 @@ public class Simulate extends Query {
         List<String> ret = new ArrayList<>();
         String ss = "SELECT ";
         String combinedColNames = String.join("||\"--\"||", parsedInputs.get("COLNAMES").split(","));
+        String newFieldName = String.join("-", parsedInputs.get("COLNAMES").split(","));
         ss += combinedColNames;
-        ss += " AS \"newFieldName\" COUNT(\"newFieldName\") AS frequency FROM (";
+        ss += " AS " + newFieldName + " COUNT(" + newFieldName + ") AS frequency FROM (";
         ss += " SIMULATE " + parsedInputs.get("COLNAMES");
         ss += " FROM " + parsedInputs.get("POPULATION");
         if (super.fields.contains("GIVEN")) {
@@ -47,7 +47,7 @@ public class Simulate extends Query {
         } else {
             ss += " LIMIT 5000";
         }
-        ss += " ) GROUP BY \"newFieldName\"";
+        ss += " ) GROUP BY " + newFieldName;
         ss += " ORDER BY frequency DESC";
         if (super.fields.contains("LIMIT2")) {
             ss += " LIMIT " + parsedInputs.get("LIMIT2");
