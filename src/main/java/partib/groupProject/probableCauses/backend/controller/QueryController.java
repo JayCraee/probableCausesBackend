b017035f63e4c864fa82012a37dde03e3064a6ea
Dan <djh242@cam.ac.uk>
2019-02-22 22:03:39 +0000

    Update database name (crime.bdb is our testing database environment)

--- src/main/java/partib/groupProject/probableCauses/backend/controller/QueryController.java
+++ src/main/java/partib/groupProject/probableCauses/backend/controller/QueryController.java
@@ -14,39 +14,39 @@ import partib.groupProject.probableCauses.backend.model.bql.query.Simulate;
 @RestController
 @RequestMapping("/bql/query")
 public class QueryController {
-
+    private final String db = "crime.bdb";
     private final Logger log = LoggerFactory.getLogger(QueryController.class);
 
     @GetMapping("/estimate/{unparsed}")
     String getEstimate(@PathVariable String unparsed) throws InvalidCallException {
         System.out.println(unparsed);
         Estimate query = new Estimate(unparsed);
-        return ServerConnector.queryCaller("foo.bdb", query.getBQL());
+        return ServerConnector.queryCaller(db, query.getBQL());
     }
 
     @GetMapping("/infer/{unparsed}")
     String getInfer(@PathVariable String unparsed) throws InvalidCallException {
         System.out.println(unparsed);
         Infer query = new Infer(unparsed);
-        return ServerConnector.queryCaller("foo.bdb", query.getBQL());
+        return ServerConnector.queryCaller(db, query.getBQL());
     }
 
     @GetMapping("/simulate/{unparsed}")
     String getSimulate(@PathVariable String unparsed) throws InvalidCallException {
         System.out.println(unparsed);
         Simulate query = new Simulate(unparsed);
-        return ServerConnector.queryCaller("foo.bdb", query.getBQL());
+        return ServerConnector.queryCaller(db, query.getBQL());
     }
 
     @GetMapping("/select/{unparsed}")
     String getSelect(@PathVariable String unparsed) throws InvalidCallException {
         System.out.println(unparsed);
         Select query = new Select(unparsed);
-        return ServerConnector.queryCaller("foo.bdb", query.getBQL());
+        return ServerConnector.queryCaller(db, query.getBQL());
     }
 
     @GetMapping("/selectTest/{parsed}")
     String getSelectTest(@PathVariable String parsed) throws InvalidCallException {
-        return ServerConnector.fooQueryCaller("SELECT * FROM sys.tables");
+        return ServerConnector.singleQueryCaller(db, "SELECT * FROM sys.tables");
     }
 }
